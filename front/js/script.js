e270a7849f8a06f690649414fff185d7acf66858
Loïc <monzaemon77.ll@gmail.com>
2021-12-03 13:09:08 +0100

    Affichage produit dans l'accueil

---
+++ front/js/script.js
@@ -0,0 +1,15 @@
+fetch("http://localhost:3000/api/products")
+  .then(function (res) {
+    if (res.ok) {
+      return res.json();
+    }
+  })
+  .then(function (data) {
+    console.log(data);
+    let display = "";
+    for (products of data) {
+      display += `<a href="./product.html?id=${products._id}"><article><img src="${products.imageUrl}" alt="${products.altTxt}"><h3 class="productName">${products.name}</h3><p class="productDescription">${products.description}</p></article></a>`;
+    }
+    document.querySelector("#items").innerHTML = display;
+  })
+  .catch(function (err) {});
