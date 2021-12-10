79a89637ce4058e063dafd083bd9ecd67e4f7c05
Loïc <monzaemon77.ll@gmail.com>
2021-12-10 15:54:32 +0100

    Modification data in LocalStorage and add Product in Cart

---
+++ front/js/cart.js
@@ -0,0 +1,29 @@
+let order = JSON.parse(localStorage.getItem("productChoose"));
+if (order === null) {
+} else {
+  let display = "";
+  for (let i in order) {
+    display += `<article class="cart__item" data-id="${order[i].idProduct}" data-color="{product-color}">
+                  <div class="cart__item__img">
+                    <img src="${order[i].imageUrl}" alt="${order[i].altTxt}">
+                  </div>
+                  <div class="cart__item__content">
+                    <div class="cart__item__content__description">
+                      <h2>${order[i].name}</h2>
+                      <p>${order[i].colorChoose}</p>
+                      <p>${order[i].price}</p>
+                    </div>
+                    <div class="cart__item__content__settings">
+                      <div class="cart__item__content__settings__quantity">
+                        <p>Qté : </p>
+                        <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${order[i].quantityChoose}">
+                      </div>
+                      <div class="cart__item__content__settings__delete">
+                        <p class="deleteItem">Supprimer</p>
+                      </div>
+                    </div>
+                  </div>
+                </article>`;
+    document.querySelector("#cart__items").innerHTML = display;
+  }
+}
--- front/js/product.js
+++ front/js/product.js
@@ -24,66 +24,70 @@ fetch(`http://localhost:3000/api/products/${id}`)
       option_color.innerHTML = `${color}`;
       document.querySelector("#colors").appendChild(option_color);
     }
+    // Recuperation des données saisie par l'user
+    const quantity_product = document.querySelector("#quantity");
+    const colorForm = document.querySelector("#colors");
+    const send = document.querySelector("#addToCart");
+    send.addEventListener("click", (event) => {
+      const quantity = quantity_product.value;
+      const choose = colorForm.value;
+      let option_product = {
+        idProduct: id,
+        name: product.name,
+        price: product.price,
+        imageUrl: product.imageUrl,
+        altTxt: product.altTxt,
+        colorChoose: choose,
+        quantityChoose: quantity,
+      };
+      // Verification du choix de l'user
+      if (choose == "") {
+        alert((message = "Veuillez choisir une couleur"));
+        return false;
+      } else if (quantity == "0") {
+        alert((message = "veuillez choisir une quantité"));
+        return false;
+      }
+      // Fonction affichage d'une alerte
+      const valid = () => {
+        alert((message = "Article(s) ajouté(s) au(x) Panier"));
+      };
+
+      // Recuperation et stockage des data saisie dans le local storage
+
+      // Verifie si y a deja un panier
+      let order = JSON.parse(localStorage.getItem("productChoose"));
+      // Vrai
+      if (order) {
+        //Verifie si l'id ET la couleur sont deja dans le tableau (productChoose)
+        const InArray = order.find(
+          (el) =>
+            el.idProduct === option_product.idProduct &&
+            el.colorChoose === option_product.colorChoose
+        );
+        // Condition Vrai
+        if (InArray) {
+          let newQuantity =
+            parseInt(option_product.quantityChoose) +
+            parseInt(InArray.quantityChoose);
+          InArray.quantityChoose = newQuantity;
+          localStorage.setItem("productChoose", JSON.stringify(order));
+          valid();
+        }
+        // Condition Fausse
+        else {
+          order.push(option_product);
+          localStorage.setItem("productChoose", JSON.stringify(order));
+          valid();
+        }
+      }
+      // Faux, on crée le panier et on y met le produit
+      else {
+        order = [];
+        order.push(option_product);
+        localStorage.setItem("productChoose", JSON.stringify(order));
+        valid();
+      }
+    });
   })
   .catch(function (err) {});
-// Recuperation des données saisie par l'user
-const quantity_product = document.querySelector("#quantity");
-const colorForm = document.querySelector("#colors");
-const send = document.querySelector("#addToCart");
-send.addEventListener("click", (event) => {
-  const quantity = quantity_product.value;
-  const choose = colorForm.value;
-  let option_product = {
-    idProduct: id,
-    colorChoose: choose,
-    quantityChoose: quantity,
-  };
-  // Verification du choix de l'user
-  if (choose == "") {
-    alert((message = "Veuillez choisir une couleur"));
-    return false;
-  } else if (quantity == "0") {
-    alert((message = "veuillez choisir une quantité"));
-    return false;
-  }
-  // Fonction affichage d'une alerte
-  const valid = () => {
-    alert((message = "Article(s) ajouté(s) au(x) Panier"));
-  };
-
-  // Recuperation et stockage des data saisie dans le local storage
-
-  // Verifie si y a deja un panier
-  let order = JSON.parse(localStorage.getItem("product"));
-  // Vrai
-  if (order) {
-    //Verifie si l'id ET la couleur sont deja dans le tableau (product)
-    const InArray = order.find(
-      (el) =>
-        el.idProduct === option_product.idProduct &&
-        el.colorChoose === option_product.colorChoose
-    );
-    // Condition Vrai
-    if (InArray) {
-      let newQuantity =
-        parseInt(option_product.quantityChoose) +
-        parseInt(InArray.quantityChoose);
-      InArray.quantityChoose = newQuantity;
-      localStorage.setItem("product", JSON.stringify(order));
-      valid();
-    }
-    // Condition Fausse
-    else {
-      order.push(option_product);
-      localStorage.setItem("product", JSON.stringify(order));
-      valid();
-    }
-  }
-  // Faux, on crée le panier et on y met le produit
-  else {
-    order = [];
-    order.push(option_product);
-    localStorage.setItem("product", JSON.stringify(order));
-    valid();
-  }
-});
