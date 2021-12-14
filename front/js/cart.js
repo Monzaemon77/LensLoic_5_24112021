62eb3f1935a0224a0276e3e87ded7e622281a6aa
Loïc <monzaemon77.ll@gmail.com>
2021-12-14 10:07:33 +0100

    Add function Modif and Del product in Cart

--- front/js/cart.js
+++ front/js/cart.js
@@ -5,7 +5,7 @@ if (order === null) {
 } else {
   let display = "";
   for (let i in order) {
-    display += `<article class="cart__item" data-id="${order[i].idProduct}" data-color="{product-color}">
+    display += `<article class="cart__item" data-id="${order[i].idProduct}" data-color="${order[i].colorChoose}">
                   <div class="cart__item__img">
                     <img src="${order[i].imageUrl}" alt="${order[i].altTxt}">
                   </div>
@@ -34,3 +34,34 @@ if (order === null) {
 }
 document.querySelector("#totalQuantity").innerHTML = quantitySum;
 document.querySelector("#totalPrice").innerHTML = priceSum;
+
+let del = document.querySelectorAll(".deleteItem");
+for (let j = 0; j < del.length; j++) {
+  del[j].addEventListener("click", (event) => {
+    //On selectionne le produit auquel est lié le bouton supprimer
+    let productSelect = order[j];
+    //On garde dans le tableau tout les objets differents de celui de selectionner
+    order = order.filter((item) => item !== productSelect);
+    localStorage.setItem("productChoose", JSON.stringify(order));
+    alert("l' ou les article(s) on bien été supprimer");
+    //On refresh la page pour mettre à jour le DOM avec les nouvelles données
+    location.reload();
+  });
+}
+//Ecoute et modification des Quantités
+let input = document.querySelectorAll(".itemQuantity");
+for (let k = 0; k < del.length; k++) {
+  input[k].addEventListener("click", (event) => {
+    //On selectionne le produit auquel est lié l'input
+    let productSelect = order[k];
+    //On met la nouvelle valeur dans une variable
+    newValue = event.target.value;
+    //On modifie le DOM avec la nouvelle valeur
+    input[k].setAttribute("value", `${newValue}`);
+    //On modifie la valeur de quantité et on la réinjecte dans LocalStorage
+    productSelect.quantityChoose = newValue;
+    localStorage.setItem("productChoose", JSON.stringify(order));
+    //On refresh la page pour mettre à jour le DOM avec les nouvelles données
+    location.reload();
+  });
+}
